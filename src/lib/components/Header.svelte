b2439812dc8f2ae034d9869af545dc2a0eef0ed6
CoopTRUE <coopdog4545@gmail.com>
2023-11-03 22:12:06 -0700

    completed nav

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,83 @@
 <script lang="ts">
+  import { Button } from '$lib/components/ui/button'
+  import * as DropdownMenu from '$lib/components/ui/dropdown-menu'
+  import {
+    ChevronDown,
+    Briefcase,
+    Building2,
+    Twitter,
+    BookOpen,
+    Shield,
+    Globe,
+  } from 'lucide-svelte'
 </script>
 
 <header class="flex items-center justify-center p-4">
-  <p>Header</p>
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger asChild let:builder>
+      <Button builders={[builder]} variant="ghost" class="group">
+        About
+        <ChevronDown
+          size="1.25em"
+          class="ml-2 mt-1 transition-transform duration-150 group-data-[state='open']:-rotate-180"
+        />
+      </Button>
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content>
+      <DropdownMenu.Item href="/about">
+        <Building2 size="1.25em" class="mr-2" />
+        Company
+      </DropdownMenu.Item>
+      <DropdownMenu.Item href="/careers">
+        <Briefcase size="1.25em" class="mr-2" />
+        Careers
+      </DropdownMenu.Item>
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger asChild let:builder>
+      <Button builders={[builder]} variant="ghost" class="group">
+        Community
+        <ChevronDown
+          size="1.25em"
+          class="ml-2 mt-1 transition-transform duration-150 group-data-[state='open']:-rotate-180"
+        />
+      </Button>
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content>
+      <DropdownMenu.Item>
+        <img src="https://cdn.simpleicons.org/discord/FFFFFF" alt="" class="mr-2 w-5" />
+        Discord
+      </DropdownMenu.Item>
+      <DropdownMenu.Item>
+        <Twitter size="1.25em" class="mr-2" />
+        Twitter
+      </DropdownMenu.Item>
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger asChild let:builder>
+      <Button builders={[builder]} variant="ghost" class="group">
+        Resources
+        <ChevronDown
+          size="1.25em"
+          class="ml-2 mt-1 transition-transform duration-150 group-data-[state='open']:-rotate-180"
+        />
+      </Button>
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content>
+      <DropdownMenu.Item href="http://www.google.com">
+        <Globe size="1.25em" class="mr-2" />
+        Blog
+      </DropdownMenu.Item>
+      <DropdownMenu.Item href="http://www.google.com">
+        <BookOpen size="1.25em" class="mr-2" />
+        Documentation
+      </DropdownMenu.Item>
+      <DropdownMenu.Item href="http://www.google.com">
+        <Shield size="1.25em" class="mr-2" />
+        Audits
+      </DropdownMenu.Item>
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
 </header>
